3c961f9c78b09c14688c9c291cf518c82386136f
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:56:37 +0800

    final update

--- 13iteratorGen/Exercise6.js
+++ 13iteratorGen/Exercise6.js
@@ -1,27 +1,23 @@
-let display = () =>
-{
-    function *fibonacci() {
-        let a = 0, b = 1;
-        yield a;
-        yield b;
-        while( true ) {
-        [a, b] = [b, a+b];
-        yield b;
-        }
-        }
-        
+function *fibonacci() {
+    let a = 0, b = 1;
+    yield a;
+    yield b;
+    while( true ) {
+    [a, b] = [b, a+b];
+    yield b;
+    }
+    }
+    
 
-    function *filter( iterable, filterFunction ) {
-        for( let element of iterable ) {
-        if ( filterFunction( element ) ) yield element;
-        }
-        }
-        let evenFibonacci = filter( fibonacci(), x => x%2 === 0 );
-        
-    console.log(evenFibonacci.next())
-    console.log(evenFibonacci.next())
-    console.log(evenFibonacci.next())
-    console.log(evenFibonacci.next())
-    console.log(evenFibonacci.next())
-}
-export default display
+function *filter( iterable, filterFunction ) {
+    for( let element of iterable ) {
+    if ( filterFunction( element ) ) yield element;
+    }
+    }
+    let evenFibonacci = filter( fibonacci(), x => x%2 === 0 );
+    
+console.log(evenFibonacci.next())
+console.log(evenFibonacci.next())
+console.log(evenFibonacci.next())
+console.log(evenFibonacci.next())
+console.log(evenFibonacci.next())
