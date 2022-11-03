46df9bf92877eb70d124d654a7431e68bdf89a9d
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:32:12 +0800

    final update

--- 10forLoop/Exercise1.js
+++ 10forLoop/Exercise1.js
@@ -1,48 +1,15 @@
-// let message = 'hello';
-// for( let i in message ) {
-//     console.log( message[i] );
-// }
-// //================================
-// let message2 = 'hello';
-// for( let ch of message2 ) {
-//     console.log( ch );
-// }
-//===================================
-// let text = '\u{1F601}\u{1F43C}';
-// console.log( 'text: ', text );
-// for( let i in text ) {
-//     console.log(text[i]);
-// }
-// console.log('-----');
-// for ( let c of text ) {
-//     console.log( c );
-// }
-//====================================
-// let flights = [
-//     { source: 'Dublin', destination: 'Warsaw' },
-//     { source: 'New York', destination: 'Phoenix' }
-// ];
-// for ( let { source, destination } of flights ) {
-//     console.log( source, destination );
-// }
-// for ( let { source } of flights ) {
-//     console.log( source );
-// }
-//================= put this snippet in the console of a website ===========//
-// let divs = document.querySelectorAll( 'div' );
-// for ( let div of divs ) {
-//     let rand = Math.floor ( Math.random() * 3 );
-//     div.style.color = [ '#990000', '#009900', '#000099'][ rand ];
-// }
-//=============== put this snippet in the console of a website ===========//
-// let text = '';
-// let nodes = document.querySelectorAll( 'h1, h2, h3, h4, h5, h6' );
-// for ( let node of nodes ) {
-//     text += node.childNodes[0].textContent[0];  
-// }
-// console.log( text );
-
-let display = () => {
-    console.log("the codes are to be placed in a console of a website")
+// ================= put this snippet in the console of a website ===========//
+let divs = document.querySelectorAll( 'div' );
+for ( let div of divs ) {
+    let rand = Math.floor ( Math.random() * 3 );
+    div.style.color = [ '#990000', '#009900', '#000099'][ rand ];
 }
-export default display
+// =============== put this snippet in the console of a website ===========//
+let text = '';
+let nodes = document.querySelectorAll( 'h1, h2, h3, h4, h5, h6' );
+for ( let node of nodes ) {
+    text += node.childNodes[0].textContent[0];  
+}
+console.log( text );
+
+console.log("the codes are to be placed in a console of a website")
