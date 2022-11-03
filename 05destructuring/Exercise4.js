8b40bd191ed97fc62e678b2ed1d43013437af357
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:10:38 +0800

    final updayes

--- 05destructuring/Exercise4.js
+++ 05destructuring/Exercise4.js
@@ -1,7 +1,4 @@
-let display = () => {
-    let x = { A: [ 't', 'e', 's', 't' ] };
-    let letter = x.A[2];
-    console.log(letter)
-    console.log("display's the nth index")
-}
-export default display
+let x = { A: [ 't', 'e', 's', 't' ] };
+let letter = x.A[2];
+console.log(letter)
+console.log("display's the nth index")
