95f2bd34f08952fa230aa950fa8bb941aabda7c2
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:22:49 +0800

    final update

--- 08tailcall/Exercise2.js
+++ 08tailcall/Exercise2.js
@@ -1,17 +1,13 @@
-let display = () =>
-{
-    function fib (n) {
-        let acc1 = 5
-        let acc2 = 0
-        let sum = 1
-        if (n <= 1) return n
-        for (let i = 1; i <= n; ++i) {
+function fib (n) {
+    let acc1 = 5
+    let acc2 = 0
+    let sum = 1
+    if (n <= 1) return n
+    for (let i = 1; i <= n; ++i) {
         [acc1, acc2] = [acc1 + acc2, acc1]
-        }
-        console.log([acc1,acc2])
-        sum = acc1 + acc2
-        console.log(sum)
     }
-    fib(4)
+    console.log([acc1,acc2])
+    sum = acc1 + acc2
+    console.log(sum)
 }
-export default display
+fib(4)
