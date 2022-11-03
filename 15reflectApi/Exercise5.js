85075a0f3f36e1eb683aff77ea7706eba7d16fb1
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 14:01:50 +0800

    final update

--- 15reflectApi/Exercise5.js
+++ 15reflectApi/Exercise5.js
@@ -10,10 +10,6 @@
         key,
         attributes
     );
-    let display = () => {
-        console.log("response wasn't configurable up untill we add the attribute configurable")
-        console.log(Reflect.deleteProperty( target, key ))
-        console.log(target)
-    }
-
-export default display
+console.log("response wasn't configurable up untill we add the attribute configurable")
+console.log(Reflect.deleteProperty( target, key ))
+console.log(target)
