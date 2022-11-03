50433882157a56a8d99a64d96e3c9f746ed4edc4
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 12:54:50 +0800

    final changes

--- 02FunctionScopeConstants/Exercise1.js
+++ 02FunctionScopeConstants/Exercise1.js
@@ -4,8 +4,8 @@ let guessMe2 = 2;
     {
     try {
             console.log( guessMe1, guessMe2 ); // (A)guessMe1 = undefined, there is no value assigned, guessMe2 = undefined, uses "let" yet there is no value assigned
-        // eslint-disable-next-line no-empty
-        } catch( _ ) {}
+        } catch( _ ) { 
+            return }
         let guessMe2 = 3;
         console.log( guessMe1, guessMe2 ); // (B)guessMe1 = 1 uses the value from line 1, guessMe2 = 3, uses the value assigned inside the block
     }
@@ -17,14 +17,3 @@ console.log( guessMe1, guessMe2 ); // (C) guessMe1 = 1, guessMe2 = 2, it uses th
             console.log( guessMe1, guessMe2 ); // (E)
         };
 console.log( guessMe1, guessMe2 ); // (F) guessMe1 = 1, guessMe2 = 2, it uses the values from lines 1 and 2
-
-let display = () => {
-    console.log("(A)guessMe1 = undefined, there is no value assigned, guessMe2 = undefined, uses let yet there is no value assigned")
-    console.log("(B)guessMe1 = 1 uses the value from line 1, guessMe2 = 3, uses the value assigned inside the block")
-    console.log("(C)guessMe1 = 1, guessMe2 = 2, it uses the values from lines 1 and 2")
-    console.log("(D) inside a block with no direction")
-    console.log("")
-    console.log("(F) guessMe1 = 1, guessMe2 = 2, it uses the values from lines 1 and 2")
-}
-
-export default display
