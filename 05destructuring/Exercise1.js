5bc7b44d4f949e28db328f07c6e8e1d2f9572727
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:08:15 +0800

    final update

--- 05destructuring/Exercise1.js
+++ 05destructuring/Exercise1.js
@@ -1,7 +1,3 @@
-let display = () => 
-{
-    let text1 = "test 1", text2 = "test 2";
+let text1 = "test 1", text2 = "test 2";
     [text1 , text2] = [text2, text1]
     console.log(text1, text2)
-}
-export default display
