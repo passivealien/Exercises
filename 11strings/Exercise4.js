9ffe2ff62692d39a833b149574e0e41a303c0f8c
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:37:54 +0800

    final update

--- 11strings/Exercise4.js
+++ 11strings/Exercise4.js
@@ -1,11 +1,6 @@
-let display = () =>
-{
-    const a = 1
+const a = 1
     const b = 2
-
     if (a < b)
         console.log(`${a} is less than ${b}`)
     else
         console.log(`${b} is less than ${a}`)
-}
-export default display
