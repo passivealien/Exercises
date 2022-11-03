1243e17fdd756a9c77b97508cd8cd5ebbb4d7672
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:25:24 +0800

    final update

--- 08tailcall/Exercise4.js
+++ 08tailcall/Exercise4.js
@@ -1,25 +1,20 @@
-let display = () =>
-{
-    let AbstractUser = function() {
-        if ( new.target === AbstractUser ) {
-            throw new Error( 'Abstract class' );
-        }
-        this.accessMatrix = {};
-    };
-        AbstractUser.prototype.hasAccess = function( page ) {
-            return this.accessMatrix[ page ];
-    };
-    let SuperUser = function() {
-        AbstractUser.call( this );
-    };
-    SuperUser.prototype = Object.create( AbstractUser.prototype );
-    SuperUser.prototype.constructor = SuperUser;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    SuperUser.prototype.hasAccess = function( page ) {
-        return true;
-    };
-    //let su = new SuperUser();
-    //let au = new AbstractUser();
-    console.log(" Abstract class cannot be instantiated") 
-}
-export default display
+let AbstractUser = function() {
+    if ( new.target === AbstractUser ) {
+        throw new Error( 'Abstract class' );
+    }
+    this.accessMatrix = {};
+};
+    AbstractUser.prototype.hasAccess = function( page ) {
+        return this.accessMatrix[ page ];
+};
+let SuperUser = function() {
+    AbstractUser.call( this );
+};
+SuperUser.prototype = Object.create( AbstractUser.prototype );
+SuperUser.prototype.constructor = SuperUser;
+SuperUser.prototype.hasAccess = function( page ) {
+    return true;
+};
+//let su = new SuperUser();
+//let au = new AbstractUser();
+console.log(" Abstract class cannot be instantiated")
