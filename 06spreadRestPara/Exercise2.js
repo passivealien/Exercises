7f7356acb449a9fb14a9165d21397c96644cb50e
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:13:06 +0800

    final updates

--- 06spreadRestPara/Exercise2.js
+++ 06spreadRestPara/Exercise2.js
@@ -1,11 +1,3 @@
-//Determine the value logged to the console on Google
-//Chrome without running it. Write down the mechanism behind it
-//using your own words
-let display = () =>{
-    let f = () => [..."12345"];
-    let A = f().map( f );
-    console.table( A );
-}
-export default display
-
-
+let f = () => [..."12345"];
+let A = f().map( f );
+console.table( A );
