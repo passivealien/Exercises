65a05012b51813bc439efe29debd59c8f04610f4
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 12:57:15 +0800

    final changes

--- 02FunctionScopeConstants/Exercise2.js
+++ 02FunctionScopeConstants/Exercise2.js
@@ -1,23 +1,19 @@
-let display = () => {
-    var guessMe1 = 1;
-    let guessMe2 = 2;
-        {
-            let guessMe2 = 3;
-        try {
-                // eslint-disable-next-line no-undef
-                a = ( {guessMe1}, {guessMe2} ); 
-            // eslint-disable-next-line no-empty
-            } catch( _ ) {}
-            
-            //console.log( guessMe1, guessMe2 ); 
+var guessMe1 = 1;
+let guessMe2 = 2;
+    {
+        let guessMe2 = 3;
+    try {
+            console.log( guessMe1, guessMe2 );
+        } catch( _ ) {
+            return
         }
-    console.log( guessMe1, guessMe2 );
-        (() => {
-                var guessMe1 = 5;
-                let guessMe2 = 6;
-                console.log( guessMe1 ); 
-                console.log( guessMe1, guessMe2 );
-            })() ;
-    console.log( guessMe1, guessMe2 );
-}
-export default display
+        ( guessMe1, guessMe2 ); 
+    }
+console.log( guessMe1, guessMe2 );
+    (() => {
+            var guessMe1 = 5;
+            let guessMe2 = 6;
+            console.log( guessMe1 ); 
+            console.log( guessMe1, guessMe2 );
+        })() ;
+console.log( guessMe1, guessMe2 );
