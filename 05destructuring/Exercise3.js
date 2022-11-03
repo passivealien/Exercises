1a3a4807104109910c7e3c96ba0b167d70437dac
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:09:57 +0800

    final update

--- 05destructuring/Exercise3.js
+++ 05destructuring/Exercise3.js
@@ -1,12 +1,5 @@
-//let node = { left : { left: 3, right: 4 }, right: 5 };
-//let { loft : {}, right : val } = node; //loft = undefined   
-//console.log(node)
-let display = () =>
-{
-    let node = { left : { left: 3, right: 4 }, right: 5 };
+let node = { left : { left: 3, right: 4 }, right: 5 };
     let { loft, right : val } = node;
     console.log(node)
     console.log(loft)
     console.log(val)
-}
-export default display
