5f1f9c17ae649ff463d7e07e40376ac7d089aaeb
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 12:58:45 +0800

    final changes

--- 03defaultArguements/Exercise1.js
+++ 03defaultArguements/Exercise1.js
@@ -1,12 +1,4 @@
-//const message = function() {  
-//    console.log("done");
-//}
-//setTimeout(message, 500);
-
-let display = () => {
-    function executeCallback( callback, delay = 1000 ) {
+function executeCallback( callback, delay = 1000 ) {
         setTimeout( callback, delay );
-        }
-    executeCallback( () => console.log('Prompt after 1 second delay') );
     }
-    export default display
+executeCallback( () => console.log('Prompt after 1 second delay') );
