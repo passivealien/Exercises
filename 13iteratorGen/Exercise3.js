608f7e655662d1e7ce36821a87576f84a877d1f0
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:54:45 +0800

    final update

--- 13iteratorGen/Exercise3.js
+++ 13iteratorGen/Exercise3.js
@@ -1,33 +1,26 @@
-let display =()=> {
-    let todoList = {
-        todoItems: [],
-        *[Symbol.iterator]() {
-            yield* this.todoItems;
-        },
-        addItem( description ) {
-            this.todoItems.push( { description, done: false } );
-            return this;
-        },
-        crossOutItem( index ) {
-            if ( index < this.todoItems.length ) {
-                this.todoItems[index].done = true;
-            }
-            return this;
+let todoList = {
+    todoItems: [],
+    *[Symbol.iterator]() {
+        yield* this.todoItems;
+    },
+    addItem( description ) {
+        this.todoItems.push( { description, done: false } );
+        return this;
+    },
+    crossOutItem( index ) {
+        if ( index < this.todoItems.length ) {
+            this.todoItems[index].done = true;
         }
+        return this;
     }
-
-        todoList.addItem( 'task 1' ).addItem( 'task 2' ).crossOutItem( 0 );
-
-        let todoListGenerator = function *() {
-            yield* todoList.todoItems;
-            }
-            
-        let iterableTodoList = todoListGenerator();
-
-        for ( let item of iterableTodoList ) {
-            console.log( item );
-    }
-
 }
+    todoList.addItem( 'task 1' ).addItem( 'task 2' ).crossOutItem( 0 );
+    let todoListGenerator = function *() {
+        yield* todoList.todoItems;
+        }
+        
+    let iterableTodoList = todoListGenerator();
 
-export default display
+    for ( let item of iterableTodoList ) {
+        console.log( item );
+}
