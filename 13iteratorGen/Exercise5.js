648f6d4a940242d9898640ea5da7f5e507fcbf0c
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:56:12 +0800

    final update

--- 13iteratorGen/Exercise5.js
+++ 13iteratorGen/Exercise5.js
@@ -1,11 +1,7 @@
-let display = () =>
-{
-    var n;
+var n;
     var fib = [0, 1];
 
     for (n = 2; n <= 10; n++) {
     fib[n] = fib[n - 2] + fib[n - 1];
     console.log(fib[n]);
     }
-}
-export default display
