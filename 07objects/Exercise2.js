6f187f829f1fdf468c592523b9d886068ae01137
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:18:48 +0800

    final chnages

--- 07objects/Exercise2.js
+++ 07objects/Exercise2.js
@@ -1,20 +1,20 @@
 let basketProto = {
-        value: 0,
-        addToBasket( itemValue ) {
-        this.value += itemValue;
-        },
-        clearBasket() {
-        this.value = 0;
-        },
-        getBasketValue() {
-        return this.value;
-        },
-        pay() {
-        console.log( this.getBasketValue() + ' has been paid' );
-        }
-    };
-    basketProto.addToBasket("no one")
-    basketProto.pay()
-    basketProto.clearBasket()
-    basketProto.addToBasket("someone")
-    basketProto.pay()
+    value: 0,
+    addToBasket( itemValue ) {
+    this.value += itemValue;
+    },
+    clearBasket() {
+    this.value = 0;
+    },
+    getBasketValue() {
+    return this.value;
+    },
+    pay() {
+    console.log( this.getBasketValue() + ' has been paid' );
+    }
+};
+basketProto.addToBasket(" no one")
+basketProto.pay()
+basketProto.clearBasket()
+basketProto.addToBasket(" someone")
+basketProto.pay()
