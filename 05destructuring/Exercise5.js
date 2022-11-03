a6b4dccf8533bfbecae37fb1391d702252a7a977
27 black gallery <114149594+passivealien@users.noreply.github.com>
2022-11-03 13:11:35 +0800

    final changes

--- 05destructuring/Exercise5.js
+++ 05destructuring/Exercise5.js
@@ -1,12 +1,9 @@
-let display = () => {
-    function drawChart( config, {
-            chartType = 0,
-            bullColor = 'green',
-            bearColor = 'red',
-            days = 30 } ) {
-                console.log( chartType, bullColor, bearColor, days );
+function drawChart( config, {
+        chartType = 0,
+        bullColor = 'green',
+        bearColor = 'red',
+        days = 30 } ) {
+            console.log( chartType, bullColor, bearColor, days );
         }
-    drawChart( [], {} );
-    drawChart( [], { chartType: 1, days: 60 } );
-}
-export default display
+drawChart( [], {} );
+drawChart( [], { chartType: 1, days: 60 } );
